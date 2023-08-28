b541cab1622e17f3c8232550606ea79019521ede
Casey Perno <caseyp@partyrentalltd.com>
2023-08-28 15:31:11 -0400

    remove container from Layout

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -6,9 +6,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <div className="container">
-        {children}
-      </div>
+      {children}
       <Footer />
     </>
   )
